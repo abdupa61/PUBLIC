8249c133f06dab18a1c91797433a166192b7f87b
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2021-06-23 00:44:50 +0300

    Add files via upload

--- gpio_servo.py
+++ gpio_servo.py
@@ -35,22 +35,29 @@ if output_pin is None:
 
 
 def main():
+    # Pin Setup:
+    # Board pin-numbering scheme
     GPIO.setmode(GPIO.BOARD)
     # set pin as an output pin with optional initial state of HIGH
     GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
     p = GPIO.PWM(output_pin, 50)
     val = 25
+    incr = -5
     p.start(val)
-    print("burda1")
-    time.sleep(8)
-    # Pin Setup:
-    # Board pin-numbering scheme
 
-    p.ChangeDutyCycle(val)
-    print("burda2")
-    time.sleep(8)
-    p.stop()
-    GPIO.cleanup()
+    print("PWM running. Press CTRL+C to exit.")
+    try:
+        while True:
+            time.sleep(0.25)
+            if val >= 150:
+                incr = -incr
+            if val <= 0:
+                incr = -incr
+            val += incr
+            p.ChangeDutyCycle(val)
+    finally:
+        p.stop()
+        GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
