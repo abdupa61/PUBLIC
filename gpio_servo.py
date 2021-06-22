188271e02132044133e6382acfef1b6416f9db96
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2021-06-22 23:11:27 +0300

    Add files via upload

--- gpio_servo.py
+++ gpio_servo.py
@@ -1,8 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
 import RPi.GPIO as GPIO
 import time
 
+output_pins = {
+    'JETSON_XAVIER': 18,
+    'JETSON_NANO': 33,
+    'JETSON_NX': 33,
+    'CLARA_AGX_XAVIER': 18,
+    'JETSON_TX2_NX': 32,
+}
+output_pin = output_pins.get(GPIO.model, None)
+if output_pin is None:
+    raise Exception('PWM not supported on this board')
 
-output_pin = 33
 
 def main():
     # Pin Setup:
