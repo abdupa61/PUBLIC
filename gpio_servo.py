7bb8cb26f57c3cc33560bf50ae687e89b06d9427
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2021-06-23 15:33:50 +0300

    Add files via upload

--- gpio_servo.py
+++ gpio_servo.py
@@ -41,8 +41,8 @@ def main():
     # set pin as an output pin with optional initial state of HIGH
     GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
     p = GPIO.PWM(output_pin, 50)
-    val = 100
-    incr = 25
+    val = 25
+    incr = 5
     p.start(val)
     k=0
     print("PWM running. Press CTRL+C to exit.")
