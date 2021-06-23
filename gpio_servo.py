069ed0737d2b7a6a80dd786362d858fb71d248c9
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2021-06-23 15:53:58 +0300

    Add files via upload

--- gpio_servo.py
+++ gpio_servo.py
@@ -1,54 +1,48 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
 import RPi.GPIO as GPIO
 import time
 
-output_pins = {
-    'JETSON_XAVIER': 18,
-    'JETSON_NANO': 33,
-    'JETSON_NX': 33,
-    'CLARA_AGX_XAVIER': 18,
-    'JETSON_TX2_NX': 32,
-}
-output_pin = output_pins.get(GPIO.model, None)
-if output_pin is None:
-    raise Exception('PWM not supported on this board')
-
+output_pin1 = 32
+output_pin2 = 33
 
 def main():
-    # Pin Setup:
-    # Board pin-numbering scheme
     GPIO.setmode(GPIO.BOARD)
-    # set pin as an output pin with optional initial state of HIGH
-    GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
-    p = GPIO.PWM(output_pin, 50)
-    p.start(25)
+    GPIO.setup(output_pin1, GPIO.OUT, initial=GPIO.HIGH)
+    p1 = GPIO.PWM(output_pin1, 50)
+    GPIO.setup(output_pin2, GPIO.OUT, initial=GPIO.HIGH)
+    p2 = GPIO.PWM(output_pin2, 50)
 
     print("PWM running. Press CTRL+C to exit.")
     try:
         while True:
+            p1.start(2.5)
+            print("p1 start at 2.5%")
+            time.sleep(1)
+            p2.start(2.5)
+            print("p2 start at 2.5%")
+            time.sleep(1)
+            p1.start(7.25)
+            print("p1 start at 7.25%")
+            time.sleep(1)
+            p2.start(7.25)
+            print("p2 start at 7.25%")
+            time.sleep(1)
+            p1.start(12)
+            print("p1 start at 12%")
+            time.sleep(1)
+            p2.start(12)
+            print("p2 start at 12%")
+            time.sleep(1)
+            p1.start(7.25)
+            print("p1 start at 7.25%")
+            time.sleep(1)
+            p2.start(7.25)
+            print("p2 start at 7.25%")
             time.sleep(1)
     finally:
-        p.stop()
+        p1.stop()
+        p2.stop()
         GPIO.cleanup()
 
 if __name__ == '__main__':
