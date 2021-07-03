2d6d49909a602e5c5181bb998f995d4b10eb6670
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2021-07-03 18:02:00 +0300

    Update motor.py

--- motor.py
+++ motor.py
@@ -5,14 +5,10 @@ def main():
     GPIO.setmode(GPIO.BCM)  
     GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
     curr_value = GPIO.HIGH
-    try:
-        while True:
-            GPIO.output(output_pin, curr_value)
-            curr_value = GPIO.HIGH
-            time.sleep(5)
-            GPIO.output(output_pin, GPIO.LOW)
-
-    finally:
-        GPIO.cleanup()
+    GPIO.output(output_pin, curr_value)
+    curr_value = GPIO.HIGH
+    time.sleep(5)
+    GPIO.output(output_pin, GPIO.LOW)
+    GPIO.cleanup()
 if __name__ == '__main__':
     main()
