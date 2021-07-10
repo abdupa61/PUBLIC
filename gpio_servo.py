44090db7cc287eb8d4e29a94f58613c996aa3be3
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2021-07-10 10:05:21 +0300

    Update gpio_servo.py

--- gpio_servo.py
+++ gpio_servo.py
@@ -6,17 +6,15 @@ import time
 output_pin2 = 33
 
 def main():
-    for i in range(5):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(output_pin2, GPIO.OUT, initial=GPIO.HIGH)
-        p2 = GPIO.PWM(output_pin2, 50)
-        p2.start(12)
-        time.sleep(0.75)
-        p2.start(7)
-        time.sleep(0.2)
-
-        p2.stop()
-        GPIO.cleanup()
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(output_pin2, GPIO.OUT, initial=GPIO.HIGH)
+    p2 = GPIO.PWM(output_pin2, 50)
+    p2.start(12)
+    time.sleep(0.75)
+    p2.start(2)
+    time.sleep(0.75)
+    p2.stop()
+    GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
