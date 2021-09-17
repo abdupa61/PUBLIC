42ee04ce2c10af991b0c9dc17e84fb32377e33d2
Abdulsamet TÜTÜNCÜ <61270806+abdupa61@users.noreply.github.com>
2021-09-17 16:10:27 +0300

    Update motor.py

--- motor.py
+++ motor.py
@@ -6,6 +6,6 @@ GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
 curr_value = GPIO.HIGH
 GPIO.output(output_pin, curr_value)
 curr_value = GPIO.HIGH
-time.sleep(25)
+time.sleep(40)
 GPIO.output(output_pin, GPIO.LOW)
 GPIO.cleanup()
